d0abe4b165f4932bd40a82501b7d2a82cf699bfd
Bruno Görß <39590058+Katalam@users.noreply.github.com>
2024-02-16 10:09:29 +0100

    rem: remove views and migrations from skeleton

--- src/LinguistServiceProvider.php
+++ src/LinguistServiceProvider.php
@@ -10,16 +10,9 @@ class LinguistServiceProvider extends PackageServiceProvider
 {
 	public function configurePackage(Package $package): void
 	{
-		/*
-		 * This class is a Package Service Provider
-		 *
-		 * More info: https://github.com/spatie/laravel-package-tools
-		 */
 		$package
 			->name('linguist')
 			->hasConfigFile()
-			->hasViews()
-			->hasMigration('create_linguist_table')
 			->hasCommand(LinguistCommand::class);
 	}
 }
