828dfb9bc51e69ae19ddf4f710874eb978459872
Ruben Maurer <aldr0n@protonmail.com>
2026-03-11 14:12:33 +0100

    feat(LGT-167): Register new actions and API client in LinguistServiceProvider

--- src/LinguistServiceProvider.php
+++ src/LinguistServiceProvider.php
@@ -2,7 +2,14 @@
 
 namespace Hyperlinkgroup\Linguist;
 
+use Hyperlinkgroup\Linguist\Actions\CollectLocalTranslations;
+use Hyperlinkgroup\Linguist\Actions\PersistLinguistConfig;
+use Hyperlinkgroup\Linguist\Actions\PullTranslations;
+use Hyperlinkgroup\Linguist\Actions\PushTranslations;
+use Hyperlinkgroup\Linguist\Actions\SyncTranslations;
 use Hyperlinkgroup\Linguist\Commands\LinguistCommand;
+use Hyperlinkgroup\Linguist\Commands\LinguistSetupCommand;
+use Hyperlinkgroup\Linguist\Services\LinguistApiClient;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
@@ -13,15 +20,40 @@ class LinguistServiceProvider extends PackageServiceProvider
 		$package
 			->name('linguist')
 			->hasConfigFile()
-			->hasCommand(LinguistCommand::class);
+			->hasCommands([
+				LinguistCommand::class,
+				LinguistSetupCommand::class,
+			]);
+	}
+
+	public function registeringPackage(): void
+	{
+		parent::registeringPackage();
+
+		// Register legacy Linguist class
+		$this->app->singleton(Linguist::class, function () {
+			return new Linguist();
+		});
+
+		// Register API Client
+		$this->app->singleton(LinguistApiClient::class, function () {
+			return new LinguistApiClient(
+				baseUrl: config('linguist.url', 'https://api.linguist.eu/'),
+				token: config('linguist.token', ''),
+				projectSlug: config('linguist.project', ''),
+			);
+		});
+
+		// Register Actions as singletons
+		$this->app->singleton(PersistLinguistConfig::class);
+		$this->app->singleton(CollectLocalTranslations::class);
+		$this->app->singleton(PullTranslations::class);
+		$this->app->singleton(PushTranslations::class);
+		$this->app->singleton(SyncTranslations::class);
 	}
 
 	public function boot(): void
 	{
 		parent::boot();
-
-		$this->app->singleton(Linguist::class, function () {
-			return new Linguist();
-		});
 	}
 }
