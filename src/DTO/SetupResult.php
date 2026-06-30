58da04c32682b5123b1ddba38b12b287ed71ad95
kaiserkiwi <kaiserkiwi@users.noreply.github.com>
2026-06-30 14:29:12 +0000

    chore(Pint): Code formatting

--- src/DTO/SetupResult.php
+++ src/DTO/SetupResult.php
@@ -6,20 +6,20 @@ namespace Hyperlinkgroup\Linguist\DTO;
 
 final class SetupResult
 {
-    /**
-     * @param array<string> $errors
-     */
-    public function __construct(
-        public readonly bool $configPersisted = false,
-        public readonly bool $projectCreated = false,
-        public readonly ?string $projectSlug = null,
-        public readonly ?SyncResult $syncResult = null,
-        public readonly bool $autoTranslate = false,
-        public readonly array $errors = [],
-    ) {}
+	/**
+	 * @param  array<string>  $errors
+	 */
+	public function __construct(
+		public readonly bool $configPersisted = false,
+		public readonly bool $projectCreated = false,
+		public readonly ?string $projectSlug = null,
+		public readonly ?SyncResult $syncResult = null,
+		public readonly bool $autoTranslate = false,
+		public readonly array $errors = [],
+	) {}
 
-    public function hasErrors(): bool
-    {
-        return $this->errors !== [];
-    }
+	public function hasErrors(): bool
+	{
+		return $this->errors !== [];
+	}
 }
